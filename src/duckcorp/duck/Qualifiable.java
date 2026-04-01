73f2bc7fde20a31059f816ebd494c9ab877b4083
Skey30 <150328180+Skey30@users.noreply.github.com>
2026-04-01 09:39:16 +0200

    interface Qualifiable

--- src/duckcorp/duck/Qualifiable.java
+++ src/duckcorp/duck/Qualifiable.java
@@ -4,7 +4,8 @@ package duckcorp.duck;
  * Interface représentant un objet dont on peut évaluer la qualité.
  *
  * TODO (Ex1) :
- *   - Implémentez isDefective() et getQualityLabel() (méthodes default)
+ * - Implémentez isDefective() et getQualityLabel() (méthodes default)
+ * 
  * @author Roussille Philippe <roussille@3il.fr>
  */
 public interface Qualifiable {
@@ -12,20 +13,18 @@ public interface Qualifiable {
     /** Retourne le score de qualité (entre 0 et 100). */
     int getQualityScore();
 
-    /**
-     * Retourne true si le score de qualité est strictement inférieur à 20.
-     * TODO : implémentez cette méthode default en vous appuyant sur getQualityScore().
-     */
     default boolean isDefective() {
-        throw new UnsupportedOperationException("TODO : Qualifiable.isDefective()");
+        return getQualityScore() < 20;
     }
 
-    /**
-     * Retourne un libellé décrivant la qualité :
-     *   score >= 80 -> "Excellent" / >= 50 -> "Bon" / >= 20 -> "Médiocre" / < 20 -> "Défectueux"
-     * TODO : implémentez cette méthode default.
-     */
     default String getQualityLabel() {
-        throw new UnsupportedOperationException("TODO : Qualifiable.getQualityLabel()");
+        int score = getQualityScore();
+        if (score >= 80)
+            return "Excellent";
+        if (score >= 50)
+            return "Bon";
+        if (score >= 20)
+            return "Médiocre";
+        return "Défectueux";
     }
 }
